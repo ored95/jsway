929e5e05bed8e404d721e4e96a8394da16adfa2f
Binh D. Nguyen <stepup.ored@gmail.com>
2022-04-16 00:58:56 +0300

    update chapter 16 React to events (v2: add event Enter key, event remove dessert and rename dessert)

--- thejsway-code/chapter16/js/03.js
+++ thejsway-code/chapter16/js/03.js
@@ -1,5 +1,24 @@
-document.getElementById("addButton").addEventListener("click", e => {
+const addDessert = e => {
     const dessert = document.createElement("li");
-    dessert.textContent = prompt("Name a desert:");
+    dessert.innerText = prompt("Name a dessert:");
+    dessert.id = dessert.innerText;
     document.getElementById("desserts").appendChild(dessert);
+};
+
+document.getElementById("addButton").addEventListener("click", addDessert);
+document.addEventListener("keypress", e => {
+    if (e.keyCode === 13)   // Enter key
+        addDessert();
+});
+
+document.addEventListener("click", e => {
+    if (e.target.tagName === "LI") {
+        document.getElementById(e.target.id).innerText = prompt("Rename that dessert by:");
+    }
+});
+
+document.addEventListener("contextmenu", e => {
+    if (e.target.tagName === "LI") {
+        document.getElementById("desserts").removeChild(e.target);
+    }
 });
